3817a239fa2706d87c2e5787209fb97e764be6a4
Ekundayo Abiona <ekundayoabiona@gmail.com>
2019-01-19 00:02:29 +0100

    Update application development setups - Add request logging - Add auto reload with nodemon and babel-node

--- src/index.js
+++ src/index.js
@@ -1,37 +1,61 @@
 import Hapi from 'hapi';
 import HapiAuthJWT from 'hapi-auth-jwt2';
 import mongoose from 'mongoose';
-import morgan from 'morgan';
+import good from 'good';
+import dotenv from 'dotenv';
+
 import routes from './routes';
 import secret from '../config';
 import { validateToken } from './util/validations';
 
-const mongoDbUri = 'mongodb://localhost:27017/expense-tracker';
+dotenv.config();
+
+const mongoDbUri = process.env.DB_URL;
 mongoose.connect(mongoDbUri, { useNewUrlParser: true });
 mongoose.connection.on('connected', () => {
-  console.log(`app is connected to ${mongoDbUri}`);
+  console.log(`app is connected to ${mongoDbUri}`); //eslint-disable-line
 });
 mongoose.connection.on('error', err =>
-  console.log('error while connecting to mongodb', err));
+  console.log('error while connecting to mongodb', err)); //eslint-disable-line
 
 const server = new Hapi.Server({
-  host: 'localhost',
-  port: 3000,
+  host: process.env.HOST || 'localhost',
+  port: process.env.PORT || 8000,
   routes: { cors: true }
 });
 
 server.route({
   path: '/',
-  method: 'GET',
-  handler(req, h) {
-    return h.response('Welcome to HapiJS course!!');
+  method: 'POST',
+  handler(req, handler) {
+    return handler.response('Welcome to HapiJS course!!');
   }
 });
 
+const options = {
+  includes: {
+    request: ['headers', 'payload'],
+    // response: ['headers', 'payload']
+  },
+  ops: false,
+  reporters: {
+    reporter: [{
+      module: 'good-console',
+      args: [{ log: '*', response: '*', request: '*' }],
+    }, 'stdout']
+  }
+};
+
 const init = async () => {
-  await server.register({
-    plugin: HapiAuthJWT
-  });
+  await server.register([
+    {
+      plugin: HapiAuthJWT
+    },
+    {
+      plugin: good,
+      options
+    }
+  ]);
 
   server.auth.strategy('jwt', 'jwt', {
     key: secret,
@@ -42,11 +66,11 @@ const init = async () => {
   server.route(routes);
 
   await server.start();
-  console.log(`Server running at: ${server.info.uri}`);
+  console.log(`Server running at: ${server.info.uri}`); //eslint-disable-line
 };
 
 process.on('unhandledRejection', (err) => {
-  console.log(err);
+  console.log(err); //eslint-disable-line
   process.exit(1);
 });
 
